6c46e8b5bda423b272a1f4506818c0ad6b620fe2
Sergio Aragonés <sergioat7@gmail.com>
2020-02-29 20:13:45 +0100

    Created generic classes for server requests

---
+++ PhotoGallery/PhotoGallery/Code/API/Client/ErrorResponse.swift
@@ -0,0 +1,11 @@
+//
+//  ErrorResponse.swift
+//  PhotoGallery
+//
+//  Created by Sergio Aragonés on 29/02/2020.
+//  Copyright (c) 2020. All rights reserved.
+//
+
+struct ErrorResponse {
+    let error: Error?
+}
