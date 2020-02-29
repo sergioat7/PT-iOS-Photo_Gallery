0ee37d83f09deb5679700d1985fe89e370a94b7a
Sergio Aragonés <sergioat7@gmail.com>
2020-02-29 19:20:27 +0100

    Created project

---
+++ PhotoGallery/PhotoGallery/Code/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  PhotoGallery
+//
+//  Created by alumno on 29/02/2020.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ PhotoGallery/PhotoGallery/Code/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PhotoGallery
+//
+//  Created by alumno on 29/02/2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
